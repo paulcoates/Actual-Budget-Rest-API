19c2affb66d41a0121edd9236385099f2a71cba5
Paul Coates <1009430+paulcoates@users.noreply.github.com>
2025-07-31 20:45:13 +0800

    fix gitignore and remove files

--- dist/utils/config.d.ts
+++
@@ -1,7 +0,0 @@
-import { Config } from '../types';
-export declare const config: Config;
-export declare const validateConfig: () => void;
-export declare const isProduction: () => boolean;
-export declare const isDevelopment: () => boolean;
-export declare const isTest: () => boolean;
-//# sourceMappingURL=config.d.ts.map
